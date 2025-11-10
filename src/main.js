fbe168311effe902cdb15d8f1fa32723da2eb45d
munyokii <munyoki912@gmail.com>
2025-11-10 19:00:39 +0300

    Used env variables

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-axios.default.baseURL = 'http://127.0.0.1:8000'
+axios.default.baseURL = process.env.BASE_URL
 
 createApp(App).use(store).use(router, axios).mount('#app')
